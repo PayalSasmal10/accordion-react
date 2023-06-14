81d236d2a38913ca6b5136d97338024b2807350a
Payal Sasmal <sasmalpayal@gmail.com>
2023-06-14 13:32:52 +0530

    accordion html and css is completed

--- src/App.js
+++ src/App.js
@@ -10,12 +10,16 @@ function App() {
           <div key={id} className="parent">
             <button className="accordion">{val.title}</button>
             <span className="openingButton">+</span>
-            <div className="panel"> 
+            <div className="panel">
               <p className="description">{val.description}</p>
             </div>
           </div>
         );
       })}
+      <div className="footer">
+        <textarea type="textarea" className="inputTextArea" />
+        <button className="addButton">Add</button>
+      </div>
     </div>
   );
 }
